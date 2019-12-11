c9d14cc371b6fd57a25e1629ab77cd34df46d11e
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-12-10 20:23:36 -0700

    added notes

--- routes/api/apiHelper.js
+++ routes/api/apiHelper.js
@@ -1,21 +1,24 @@
 const db = require("../../data/dbConfig.js");
 
 function addUser(user) {
-  return db("users")
-    .insert(user)
+  // takes an argument, expected object
+  return db("users") // addressing users table
+    .insert(user) // inserting arg object into addressed table ( users )
     .then(res => {
-      return res;
+      // initializes a promise
+      return res; // will be the id
     });
 }
 
 function findBy(key) {
-  return db("users")
-    .where(key)
-    .first();
+  // takes an arugment
+  return db("users") // addresses the users table
+    .where(key) // where the key matches the corresponding column
+    .first(); // returns the first item
 }
 
 function getUsers() {
-  return db("users");
+  return db("users"); // implies .select("*").from("users")
 }
 
 module.exports = {
--- routes/api/apiRouter.js
+++ routes/api/apiRouter.js
@@ -1,16 +1,16 @@
 const db = require("./apiHelper.js");
 const express = require("express");
-const crypt = require("bcryptjs");
+const crypt = require("bcryptjs"); // for whatever reason I can never type bcrypt on my first try
 const jwt = require("jsonwebtoken");
-const secrets = require("../../config/secrets.js");
+const secrets = require("../../config/secrets.js"); // external file so in production I can add to the gitignore
 
 const router = express.Router();
 
 router.post("/register", (req, res) => {
   const creds = req.body;
 
-  const hash = crypt.hashSync(creds.password, 8);
-  creds.password = hash;
+  const hash = crypt.hashSync(creds.password, 8); // hashes the password 8 times
+  creds.password = hash; // resets the password to the hashed string, so we don't store sensitive data
 
   db.addUser(creds)
     .then(id => {
@@ -26,10 +26,11 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-  db.findBy({ username })
+  db.findBy({ username }) // runs the filter by username
     .then(user => {
       if (user && crypt.compareSync(password, user.password)) {
-        let token = generateToken(user);
+        // if the resolution is truthy AND the password guess matches stored hash
+        let token = generateToken(user); // creates a token using JWT, go to this function
         res.status(200).json({ message: `Welcome ${user.username}!`, token });
       } else {
         res.status(401).json({ message: "Invalid credentials" });
@@ -51,15 +52,19 @@ router.get("/users", tokenAuth, (req, res) => {
 });
 
 function generateToken(user) {
+  // this is a good candidate to go in an external file and ignored
+  // must pass 3 args with .sign, payload, a secret, options obj
   const payload = {
-    subject: user.id,
-    username: user.username
+    subject: user.id, // almost always have subject line, refers to ID
+    username: user.username // for reference?
+    // ... anything else
   };
 
   const options = {
-    expiresIn: "30s"
+    expiresIn: "30s" // common option, how long the token lasts
   };
-
+  // note, objects may be passed in line; the following is legal
+  // return jwt.sign({subject: user.id}, "Give me something to shoot", {expiresIn: 9000d})
   return jwt.sign(payload, secrets.jwtSecret, options);
 }
 
@@ -67,18 +72,21 @@ function tokenAuth(req, res, next) {
   const token = req.headers.authorization;
 
   if (req.decodedJwt) {
+    // if we have a token already decoded
     next();
   } else if (token) {
     jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      //pass the current token and the secret to check the signature
       if (err) {
+        // if an error occurs
         res.status(401).json({ message: "Failed to verify authorization" });
       } else {
-        req.decodedJwt = decodedJwt;
+        req.decodedJwt = decodedJwt; // set to speed up future auth
         next();
       }
     });
   } else {
-    res.status(401).json({ message: "Failed to verify authorization" });
+    res.status(401).json({ message: "Failed to verify authorization" }); // overall failure
   }
 }
 
